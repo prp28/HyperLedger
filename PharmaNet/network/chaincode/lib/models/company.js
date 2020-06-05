65e06404adb655d8c893fde2370cc996f05052dc
praarp28 <ppodutwar@deloitte.com>
2020-06-05 17:31:21 +0530

    smart contract added

---
+++ PharmaNet/network/chaincode/lib/models/company.js
@@ -0,0 +1,67 @@
+'use strict';
+
+class Company {
+
+	/**
+	 * Constructor function
+	 * @param companyRequest {Object}
+	 */
+	constructor(companyRequest) {
+		this.key = User.makeKey([companyRequest.crn, companyRequest.name]);
+		Object.assign(this, companyRequest);
+	}
+
+	/**
+	 * Get class of this model
+	 * @returns {string}
+	 */
+	static getClass() {
+		return 'org.pharma-network.pharmanet.models.company';
+	}
+
+	/**
+	 * Convert the buffer stream received from blockchain into an object of this model
+	 * @param buffer {Buffer}
+	 */
+	static fromBuffer(buffer) {
+		let json = JSON.parse(buffer.toString());
+		return new Company(json);
+	}
+
+	/**
+	 * Convert the object of this model to a buffer stream
+	 * @returns {Buffer}
+	 */
+	toBuffer() {
+		return Buffer.from(JSON.stringify(this));
+	}
+
+	/**
+	 * Create a key string joined from different key parts
+	 * @param keyParts {Array}
+	 * @returns {*}
+	 */
+	static makeKey(keyParts) {
+		return keyParts.map(part => JSON.stringify(part)).join(":");
+	}
+
+	/**
+	 * Create an array of key parts for this model instance
+	 * @returns {Array}
+	 */
+	getKeyArray() {
+		return this.key.split(":");
+	}
+
+	/**
+	 * Create a new instance of this model
+	 * @returns {User}
+	 * @param userObject {Object}
+	 */
+	static createInstance(companyObject) {
+		return new Company(companyObject);
+	}
+
+}
+
+module.exports = Company;
--- property-registration/chaincode/index.js
+++ property-registration/chaincode/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const usercontract = require('./usercontract.js');
-const registrarcontract = require('./registrarcontract.js');
+const usercontract = require('./pharmacontract.js');
+// const registrarcontract = require('./registrarcontract.js');
 module.exports.contracts = [usercontract, registrarcontract];
